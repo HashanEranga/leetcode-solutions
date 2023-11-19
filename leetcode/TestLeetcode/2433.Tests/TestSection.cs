2daa7e36b44411ad6ad0c5e329eff44f630b2a7d
HashanEranga <ccmrhe@gmail.com>
2023-11-20 02:42:25 +0530

    2433. Find The Original Array of Prefix Xor

---
+++ leetcode/TestLeetcode/2433.Tests/TestSection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._2433.Tests
+{
+    public class TestSection
+    {
+        private leetcode.Arrays._2433._Find_The_Original_Array_of_Prefix_Xor.Solution findArray;
+        [SetUp]
+        public void SetUp() 
+        { 
+            findArray = new leetcode.Arrays._2433._Find_The_Original_Array_of_Prefix_Xor.Solution();
+        }
+
+        [Test]
+        [TestCase(new int[] { 5, 2, 0, 3, 1 }, new int[] { 5, 7, 2, 3, 2 })]
+        [TestCase(new int[] { 13 }, new int[] { 13 })]
+        [TestCase(new int[] { }, new int[] { })]
+        public void FindArray_WhenPrefArrayAdded_ReturnOriginalArrayOfXOR(int[] input, int[] expectedOutput)
+        {
+            var ans = findArray.FindArray(input);
+
+            Assert.That(ans, Is.Not.Null);
+            Assert.That(ans.Length, Is.EqualTo(expectedOutput.Length));
+            Assert.That(ans, Is.EqualTo(expectedOutput));
+        }
+
+
+    }
+}
---
+++ leetcode/leetcode/Arrays/2433. Find The Original Array of Prefix Xor/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays._2433._Find_The_Original_Array_of_Prefix_Xor
+{
+    public class Solution
+    {
+        public int[] FindArray(int[] pref)
+        {
+            var ans = new int[pref.Length];
+            int rem = 0;
+            for(int i=0; i< pref.Length; i++)
+            {
+                if (i == 0) {
+                    ans[i] = pref[i];
+                    rem = ans[i];
+                }
+                else
+                {
+                    ans[i] = rem ^ pref[i];
+                    rem = rem ^ ans[i];
+                }
+            }
+            return ans;
+        }
+    }
+}
