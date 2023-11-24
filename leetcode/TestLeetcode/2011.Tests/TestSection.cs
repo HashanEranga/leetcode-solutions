764e6168c270aea978bf2f3d2db9f0d88b08775f
HashanEranga <ccmrhe@gmail.com>
2023-11-24 14:04:30 +0530

    2011. Final Value of Variable After Performing Operations

---
+++ leetcode/TestLeetcode/2011.Tests/TestSection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._2011.Tests
+{
+    public class TestSection
+    {
+        private leetcode.Arrays._2011._Final_Value_of_Variable_After_Performing_Operations.Solution _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new leetcode.Arrays._2011._Final_Value_of_Variable_After_Performing_Operations.Solution();
+        }
+
+        [Test]
+        [TestCase(new string[] {"++X", "++X", "++X"}, 3)]
+        [TestCase(new string[] { "--X", "X++", "X++" }, 1)]
+        [TestCase(new string[] { "++X", "++X", "X++" }, 3)]
+        [TestCase(new string[] { "X++", "++X", "--X", "X--" }, 0)]
+        [TestCase(new string[] {}, 0)]
+        public void FinalValueAfterOperations_AddedAnOperationArray_ReturnTheValueAfterOperations(string[] operations, int expectedAns)
+        {
+            var ans = _solution.FinalValueAfterOperations(operations);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(expectedAns));
+        }
+
+        [Test]
+        [TestCase(new string[] { "++X", "++X", "++X" }, 3)]
+        [TestCase(new string[] { "--X", "X++", "X++" }, 1)]
+        [TestCase(new string[] { "++X", "++X", "X++" }, 3)]
+        [TestCase(new string[] { "X++", "++X", "--X", "X--" }, 0)]
+        [TestCase(new string[] { }, 0)]
+        public void FinalValueAfterOperationsAlternate_AddedAnOperationArray_ReturnTheValueAfterOperations(string[] operations, int expectedAns)
+        {
+            var ans = _solution.FinalValueAfterOperations_Alternative(operations);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(expectedAns));
+        }
+    }
+}
---
+++ leetcode/leetcode/Arrays/2011. Final Value of Variable After Performing Operations/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays._2011._Final_Value_of_Variable_After_Performing_Operations
+{
+    public class Solution
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            var currentValue = 0;
+            foreach(var item in operations)
+            {
+                switch (item)
+                {
+                    case "++X":
+                        currentValue++; break;
+                    case "X++":
+                        currentValue++; break;
+                    case "--X":
+                        currentValue--; break;
+                    case "X--":
+                        currentValue--; break;
+                    default:
+                        break;
+                }
+            }
+            return currentValue;
+        }
+
+        public int FinalValueAfterOperations_Alternative(string[] operations)
+        {
+            var currentValue = 0;
+            foreach(var item in operations)
+            {
+                if (item == "++X" || item == "X++") currentValue++;
+                else if (item == "--X" || item == "X--") currentValue--;
+            }
+            return currentValue;
+        }
+    }
+}
