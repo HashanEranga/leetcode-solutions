0f90629f393726d63822b9849589d00560ea6a04
HashanEranga <ccmrhe@gmail.com>
2023-11-24 08:29:17 +0530

    1512. Number of Good Pairs

---
+++ leetcode/TestLeetcode/1512.Tests/TestSection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._1512.Tests
+{
+    public class TestSection
+    {
+        private leetcode.Arrays._1512._Number_of_Good_Pairs.Solution Solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Solution = new leetcode.Arrays._1512._Number_of_Good_Pairs.Solution();
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 1, 1, 3 }, 4)]
+        [TestCase(new int[] { 1, 1, 1, 1 }, 6)]
+        public void NumIdenticalPairs_InputNonEmptyArray_ReturnNumberofGoodPairs(int[] inputArr, int expectedAns)
+        {
+            var ans = Solution.NumIdenticalPairs(inputArr);
+
+            Assert.That(ans, Is.GreaterThan(0));
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(expectedAns));
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3})]
+        public void NumIdenticalPairs_InputNonEmptyArrayWithNoGoodPairs_ReturnNumberofGoodPairsAsZero(int[] inputArr)
+        {
+            var ans = Solution.NumIdenticalPairs(inputArr);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(0));
+        }
+    }
+}
---
+++ leetcode/leetcode/Arrays/1512. Number of Good Pairs/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays._1512._Number_of_Good_Pairs
+{
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if (i<j && nums[j] == nums[i]) count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
