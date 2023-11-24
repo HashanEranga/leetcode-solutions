23c10f76645127cc390545697dcf79f73c24d0ab
HashanEranga <ccmrhe@gmail.com>
2023-11-24 17:24:20 +0530

    2798. Number of Employees Who Met the Target

---
+++ leetcode/TestLeetcode/2798.Tests/TestSection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._2798.Tests
+{
+    public class TestSection
+    {
+        private leetcode.Arrays._2798._Number_of_Employees_Who_Met_the_Target.Solution solution;
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new leetcode.Arrays._2798._Number_of_Employees_Who_Met_the_Target.Solution();
+        }
+
+        [Test]
+        [TestCase(new int[] { 0, 1, 2, 3, 4 }, 2, 3)]
+        [TestCase(new int[] { 5, 1, 4, 2, 2 }, 6, 0)]
+        [TestCase(new int[] { }, 6, 0)]
+        public void NumberOfEmployeesWhoMetTargetTest_InputHours_ReturnCountOfEmployeeWhoGotTarget(int[] hours, int target, int expectedOutcome)
+        {
+            var ans = solution.NumberOfEmployeesWhoMetTarget(hours, target);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(expectedOutcome));
+        }
+    }
+}
---
+++ leetcode/leetcode/Arrays/2798. Number of Employees Who Met the Target/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays._2798._Number_of_Employees_Who_Met_the_Target
+{
+    public class Solution
+    {
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            var count = 0;
+            foreach(var item in hours)
+            {
+                if(item >= target) count++;
+            }
+            return count;
+        }
+    }
+}
