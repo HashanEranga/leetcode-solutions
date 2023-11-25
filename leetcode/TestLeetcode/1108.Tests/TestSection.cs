4e25487af266321a73ac80d099755122f1b422ce
HashanEranga <ccmrhe@gmail.com>
2023-11-25 22:13:56 +0530

    1108. Defanging an IP Address

---
+++ leetcode/TestLeetcode/1108.Tests/TestSection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._1108.Tests
+{
+    public class TestSection
+    {
+        private leetcode.String._1108._Defanging_an_IP_Address.Solution solution;
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new leetcode.String._1108._Defanging_an_IP_Address.Solution();
+        }
+
+        [Test]
+        [TestCase("1.1.1.1", "1[.]1[.]1[.]1")]
+        [TestCase("255.100.50.0", "255[.]100[.]50[.]0")]
+        public void DefangIPaddr_AddAddress_ReturnWithReplacedChars(string address, string expectedAddress)
+        {
+            var ans = solution.DefangIPaddr(address);
+
+            Assert.That(ans, Is.Not.Null);
+            Assert.That(ans, Is.EqualTo(expectedAddress));
+        }
+    }
+}
---
+++ leetcode/leetcode/String/1108. Defanging an IP Address/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String._1108._Defanging_an_IP_Address
+{
+    public class Solution
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
