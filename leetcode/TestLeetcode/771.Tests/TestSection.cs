63e1604ac9a5e0df4ea94fe1064be95504674f77
HashanEranga <ccmrhe@gmail.com>
2023-12-03 09:38:15 +0530

    771. Jewels and Stones

---
+++ leetcode/TestLeetcode/771.Tests/TestSection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._771.Tests
+{
+    public class TestSection
+    {
+        private leetcode.String._771._Jewels_and_Stones.Solution _solution;
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new leetcode.String._771._Jewels_and_Stones.Solution();
+        }
+
+        [Test]
+        [TestCase("aA", "aAAbbbb", 3)]
+        [TestCase("z", "ZZ", 0)]
+        public void NumJewelsInStones_EnterJewels_ReturnNumberOfJewelStones(string jewels, string stones, int expectedOutput)
+        {
+            var ans = _solution.NumJewelsInStones(jewels, stones);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(expectedOutput));
+        }
+    }
+}
---
+++ leetcode/leetcode/String/771. Jewels and Stones/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String._771._Jewels_and_Stones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+            foreach(var jewel in jewels)
+            {
+                var currentCount = stones.Count(x=>x.Equals(jewel));
+                count += currentCount;
+            }
+
+            return count;
+        }
+    }
+}
