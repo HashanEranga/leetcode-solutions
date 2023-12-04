b930ae5e73080686a9fdd5ab9d4d19bf4ccf1622
HashanEranga <ccmrhe@gmail.com>
2023-12-04 16:00:11 +0530

    1678. Goal Parser Interpretation

---
+++ leetcode/TestLeetcode/1678.Tests/TestSection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._1678.Tests
+{
+    public class TestSection
+    {
+        private leetcode.String._1678._Goal_Parser_Interpretation.Solution solution;
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new leetcode.String._1678._Goal_Parser_Interpretation.Solution();
+        }
+
+        [Test]
+        [TestCase("G()(al)", "Goal")]
+        [TestCase("G()()()()(al)", "Gooooal")]
+        [TestCase("(al)G(al)()()G", "alGalooG")]
+        public void Interpret_DefaultCase_ReturnCorrectWord(string input, string expectedResult)
+        {
+            var ans = solution.Interpret(input);
+
+            Assert.That(ans, Is.Not.Empty);
+            Assert.That(ans, Is.Not.Null);
+            Assert.That(ans, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ leetcode/leetcode/String/1678. Goal Parser Interpretation/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String._1678._Goal_Parser_Interpretation
+{
+    public class Solution
+    {
+        public string Interpret(string command)
+        {
+            if (command.Contains("()")) command = command.Replace("()", "o");
+            if (command.Contains("(al)")) command = command.Replace("(al)", "al");
+
+            return command;
+        }
+    }
+}
