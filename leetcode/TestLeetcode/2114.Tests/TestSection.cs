4f436f7d1ae4f9ffd5f8857579c4ed0126630b45
HashanEranga <ccmrhe@gmail.com>
2023-12-06 00:40:23 +0530

    2114. Maximum Number of Words Found in Sentences

---
+++ leetcode/TestLeetcode/2114.Tests/TestSection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._2114.Tests
+{
+    public class TestSection
+    {
+        private leetcode.String._2114._Maximum_Number_of_Words_Found_in_Sentences.Solution solution;
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new leetcode.String._2114._Maximum_Number_of_Words_Found_in_Sentences.Solution();
+        }
+
+        [Test]
+        public void MostWordsFound_DefaultScenario_ReturnMaxWordCount()
+        {
+            var strArr = new string[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" };
+            var ans = solution.MostWordsFound(strArr);
+
+            Assert.That(ans, Is.TypeOf<int>()); 
+            Assert.That(ans, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void MostWordsFound_DefaultScenario2_ReturnMaxWordCount()
+        {
+            var strArr = new string[] { "please wait", "continue to fight", "continue to win" };
+            var ans = solution.MostWordsFound(strArr);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void MostWordsFound_EmptyList_ReturnZero()
+        {
+            var strArr = new string[] { };
+            var ans = solution.MostWordsFound(strArr);
+
+            Assert.That(ans, Is.TypeOf<int>());
+            Assert.That(ans, Is.EqualTo(0));
+        }
+    }
+}
---
+++ leetcode/leetcode/String/2114. Maximum Number of Words Found in Sentences/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String._2114._Maximum_Number_of_Words_Found_in_Sentences
+{
+    public class Solution
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            int maxWords = 0;
+            foreach (string sentence in sentences)
+            {
+                if (sentence != null)
+                {
+                    if(sentence.Trim().Split(' ').Count() > maxWords) maxWords = sentence.Trim().Split(" ").Count();
+                }
+            }
+            return maxWords;
+        }
+    }
+}
