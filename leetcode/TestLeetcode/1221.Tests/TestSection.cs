4d508eb052c72ce42118ff64080de7284c78a0cc
HashanEranga <ccmrhe@gmail.com>
2023-12-07 10:41:20 +0530

    1221. Split a String in Balanced Strings

---
+++ leetcode/TestLeetcode/1221.Tests/TestSection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetcode._1221.Tests
+{
+    public class TestSection
+    {
+        private leetcode.String._1221._Split_a_String_in_Balanced_Strings.Solution solution;
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new leetcode.String._1221._Split_a_String_in_Balanced_Strings.Solution();
+        }
+
+        [Test]
+        [TestCase("RLRRLLRLRL", 4)]
+        [TestCase("RLRRRLLRLL", 2)]
+        [TestCase("LLLLRRRR", 1)]
+        public void BalancedStringSplit_DefaultCaseTesting_ReturnBlancedStringSplit(string s, int expectedValue)
+        {
+            var ans = solution.BalancedStringSplit(s);
+
+            Assert.That(ans, Is.EqualTo(expectedValue));
+        }
+    }
+}
---
+++ leetcode/leetcode/String/1221. Split a String in Balanced Strings/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.String._1221._Split_a_String_in_Balanced_Strings
+{
+    public class Solution
+    {
+        public int BalancedStringSplit(string s)
+        {
+            var charArr = s.ToCharArray();
+
+            var countL = 0;
+            var countR = 0;
+            var balancedStringCount = 0;
+
+            foreach (var c in charArr)
+            {
+                if (c == 'L') countL++;
+                if (c == 'R') countR++;
+                if (countL == countR) balancedStringCount++;
+            }
+
+            return balancedStringCount;
+        }
+    }
+}
